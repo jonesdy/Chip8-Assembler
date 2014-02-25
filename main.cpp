1efc96e34e50e8f10b3ee7bce27de4d05207f3af
Dylan Jones <jonesdy@uwplatt.edu>
2014-02-24 19:37:00 -0600

    Quick style fix

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
    Assembler assembler;
 
    if(argc <= 1)
+   {
       std::cout<<"Please input a filename\n";
+   }
    else
    {
       assembler.assemble(argv[1]);
